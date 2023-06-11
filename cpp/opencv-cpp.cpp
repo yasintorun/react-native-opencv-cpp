1a839321ccfbc8c331e950b3a8e897f950b2c620
Yasin Torun <yasintorun4680@gmail.com>
2023-06-11 15:49:10 +0300

    Created basic C++ modules

---
+++ cpp/opencv-cpp.cpp
@@ -0,0 +1,8 @@
+
+#include "opencv-cpp.h"
+
+namespace opencvcpp {
+  std::string createMessage(std::string message) {
+    return "CPP: " + message;
+  }
+}
---
+++ cpp/opencv-cpp.h
@@ -0,0 +1,9 @@
+
+#ifndef OPENCVCPP_H
+#define OPENCVCPP_H
+#include <string>
+namespace opencvcpp {
+  std::string createMessage(std::string message);
+}
+
+#endif /* OPENCVCPP_H */
